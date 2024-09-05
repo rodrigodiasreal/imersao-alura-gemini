9bcf116c41da8c316d47b7a5d4697f9a396112b9
rodrigodiasreal <rodrigod.1988@outlook.com>
2024-09-05 05:51:39 -0300

    Aula 3
    Aprofundar em JavaScript;
    Organizar e estruturar melhor o seu projeto com JavaScript;
    Criar um novo arquivo JS para os dados da base de conhecimento;
    Aprender conceitos e manipulação de variáveis no JS.

---
+++ dados.js
@@ -0,0 +1,15 @@
+let dados = [{
+    titulo : "Rayssa Leal",
+    descricao: "Rayssa Leal é uma das maiores skatistas brasileiras, conhecida por sua habilidade e carisma. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil, conquistando a prata nos Jogos Olímpicos de Tóquio.",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+}, 
+{
+    titulo: "Rebeca Andrade", 
+    descricao: "Rebeca Andrade é uma das maiores ginastas brasileiras de todos os tempos. Com sua força, elegância e determinação, ela conquistou o coração dos brasileiros e se tornou um verdadeiro ícone do esporte nacional.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+}, 
+{
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a primeira medalha de ouro para o Brasil na competição. Sua força e técnica a tornaram uma das principais atletas do judô brasileiro.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza_(judoca)",
+}];
