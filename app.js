3a6d7e0cdab3e5a6811df3631da98ab5bc6bc281
rodrigodiasreal <rodrigod.1988@outlook.com>
2024-09-07 14:17:08 -0300

    Aula 4
    Aprender a deixar a aplicação JS interativa;
    Exibir todos os resultados no botão de clicar;
    Saber como funciona a premiação dos projetos dessa Imersão.

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log(dados);
+function pesquisar() {
+    // Seleciona a seção onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+    console.log(section);
+  
+    // Inicializa uma string para armazenar os resultados
+    let resultados = "";
+  
+    // Itera sobre cada dado e adiciona uma nova div com os resultados
+    for (let dado of dados) {
+      resultados += `<div class="item-resultado">
+        <h2>
+          <a href="https://www.www.instagram.com/rayssalealoficial/" target="_blank">${dado.titulo}</a>
+        </h2>
+        <p class="descricao-meta">
+          ${dado.descricao}</p>
+        <a href=${dado.link} target="_blank">Mais informações</a>
+      </div>`
+    }
+  
+    // Insere os resultados na seção
+    section.innerHTML = resultados
+  }
+
+
+
