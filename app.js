3f6b6df6c8c5c120282c872fea32227d3bbf55e1
rodrigodiasreal <rodrigod.1988@outlook.com>
2024-09-08 20:23:43 -0300

    Última aula
    Aprender a colocar o seu código no GitHub;
    Implementar a busca para mostrar apenas o resultado desejado;
    Criar lógicas para melhorar a experiência da pesquisa;
    Fazer o Deploy do projeto na Vercel.

--- app.js
+++ app.js
@@ -1,26 +1,42 @@
 function pesquisar() {
-    // Seleciona a seção onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
-    console.log(section);
-  
-    // Inicializa uma string para armazenar os resultados
-    let resultados = "";
-  
-    // Itera sobre cada dado e adiciona uma nova div com os resultados
-    for (let dado of dados) {
+  // Seleciona a seção onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+  console.log(section);
+
+  let campoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase();
+
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>Você não pesquisou nenhum atleta.</p>";
+    return;
+  }
+
+  // Inicializa uma string para armazenar os resultados
+  let resultados = "";
+  let quantidadeResultados = 0;
+
+  // Itera sobre cada dado e adiciona uma nova div com os resultados
+  for (let dado of dados) {
+    let titulo = dado.titulo.toLowerCase();
+    let descricao = dado.descricao.toLowerCase();
+    let tags = dado.tags.toLowerCase();
+
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+      quantidadeResultados++;
       resultados += `<div class="item-resultado">
         <h2>
           <a href="https://www.www.instagram.com/rayssalealoficial/" target="_blank">${dado.titulo}</a>
         </h2>
         <p class="descricao-meta">
-          ${dado.descricao}</p>
+          ${dado.descricao.replace(new RegExp(campoPesquisa, 'gi'), match => `<strong>${match}</strong>`)}</p>
         <a href=${dado.link} target="_blank">Mais informações</a>
-      </div>`
+      </div>`;
     }
-  
-    // Insere os resultados na seção
-    section.innerHTML = resultados
   }
 
-
-
+  // Verifica se foram encontrados resultados
+  if (quantidadeResultados === 0) {
+    section.innerHTML = `<p>Não encontramos nenhum atleta com o termo: "${campoPesquisa}"</p>`;
+  } else {
+    section.innerHTML = resultados;
+  }
+}
